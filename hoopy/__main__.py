a5c9f44c9bb9c8644e147a9840d5f44234c517e4
Olivia Palmu <oliviaspalmu@gmail.com>
2022-10-30 23:39:40 +0200

    Add command-line transformer as a test

--- hoopy/__main__.py
+++ hoopy/__main__.py
@@ -4,3 +4,10 @@ The `hoopy` CLI transforms and runs a module directly.
 
 import argparse
 import hoopy
+
+# Example transformer
+from hoopy.transform import transform
+import sys
+
+input = "\n".join(sys.stdin.readlines())
+print(transform(input))
